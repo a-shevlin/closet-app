f93995cb8ae722e39e3fa036e36aca745520a10c
Alex Shevlin <alexshevlin1@gmail.com>
2022-10-26 15:06:19 -0700

    WIP firebase auth connection.

--- src/screens/SignInScreen/SignInScreen.jsx
+++ src/screens/SignInScreen/SignInScreen.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import CustomInput from '../../components/CustomInput';
+import { auth } from '../../firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { colors } from '../../context/constants';
 
 
 export default function SignInScreen() {
+  const [signUpSuccess, setSignUpSuccess] = useState(null);
+
+
   return(
     <View style={styles.container}>
       <Text>Sign In Screen</Text>
