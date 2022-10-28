9a9971792357397b99bfd877fe4b7c773ee1521e
Alex Shevlin <alexshevlin1@gmail.com>
2022-10-28 00:32:30 -0700

    update to include Auth WIP.

--- src/screens/SignInScreen/SignInScreen.jsx
+++ src/screens/SignInScreen/SignInScreen.jsx
@@ -1,19 +1,45 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import CustomInput from '../../components/CustomInput';
+import { StyleSheet, View, Text, Dimensions, TouchableOpacity, TextInput } from 'react-native';
 import { auth } from '../../firebase';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword } from '@firebase/auth';
 import { colors } from '../../context/constants';
+import Add from '../../components/Add';
 
 
 export default function SignInScreen() {
   const [signUpSuccess, setSignUpSuccess] = useState(null);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  function doSignUp() {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential)
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+  }
 
   return(
     <View style={styles.container}>
-      <Text>Sign In Screen</Text>
-      <CustomInput />
+      {/* <Add /> */}
+      <TextInput 
+        style={styles.input}
+        value={email}
+        placeholder={"Email"}
+        onChangeText={text=>setEmail(text)}
+      />
+      <TextInput 
+        style={styles.input}
+        value={password}
+        placeholder={"Password"}
+        secureTextEntry={true}
+        onChangeText={text=>setPassword(text)}
+      />
+      <TouchableOpacity onPress={doSignUp}>
+        <Text>Sign Up</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -22,7 +48,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Dimensions.get('window').height / 16,
     backgroundColor: colors.darkerBG,
   },
 })
