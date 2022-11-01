2763d1c193e58ef0ce403c844ce671c8a2a83f1b
Alex Shevlin <alexshevlin1@gmail.com>
2022-11-01 00:14:08 -0700

    update app.js to show home if user is logged in.

--- App.js
+++ App.js
@@ -1,28 +1,68 @@
 // import { registerRootComponent } from 'expo';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-import SignInScreen from './src/screens/SignInScreen';
-import Home from './src/screens/Home';
+import { firebase } from './src/firebase';
+import { Home, SignUpScreen, LoginScreen } from './src/screens';
 import { colors } from './src/context/constants';
+
 
 export default function App() {
   const Stack = createNativeStackNavigator();
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  // if (loading) {	
+  //   return (	
+  //     <></>	
+  //   )	
+  // }
+
+  useEffect(() => {
+    const usersRef = firebase.firestore().collection('users');
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        usersRef
+          .doc(user.uid)
+          .get()
+          .then((document) => {
+            const userData = document.data()
+            setLoading(false)
+            setUser(userData)
+          })
+          .catch((error) => {
+            setLoading(false)
+          });
+      } else {
+        setLoading(false)
+      }
+    });
+  }, []);
 
   return (
     <NavigationContainer>
         <Stack.Navigator>
+        { user ? (
           <Stack.Screen 
             name="Home"  
             component={Home}
             options={{
-              title: "Welcome"
+              title: "Welcome",
             }}
           />
-          <Stack.Screen 
-            name="Account" 
-            component={SignInScreen}/>
+        ) : (
+          <>
+            <Stack.Screen 
+              name="Login" 
+              component={LoginScreen}/>
+            <Stack.Screen 
+              name="Register" 
+              component={SignUpScreen}/>
+          </>
+        )}
+          
+          
         </Stack.Navigator>
     </NavigationContainer>
   );
